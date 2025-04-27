46ea024193ffa5471b4be31efaa24977aa3f372e
RounitSingh <rounitsingh2002@gmail.com>
2025-04-27 15:31:23 +0530

    second commit

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { IncidentDashboard } from '@/components/IncidentDashboard';
 
 const Index = () => {
+  // sdfedf
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-800 text-gray-100">
       <IncidentDashboard />
